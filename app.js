9c26d47552a204112302178b73312a1f99192bc2
Shaban Irshad <shabanirshad408@gmail.com>
2026-09-12 11:15:57 +0530

    Started a new project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+const port=3000;
+
+app.set('view engine','ejs');
+app.use(express.static(Path2D.length))
+
+app.listen(port,(err)=>{
+    if(err){
+        console.error(err);
+    }
+    console.log('App is listening on port: ',port);
+})
