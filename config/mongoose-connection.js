62854311bc7290e22950696913a15d69b9c99866
Shaban Irshad <shabanirshad408@gmail.com>
2026-09-12 14:36:01 +0530

    created Routers

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,9 @@
 const mongoose=require('mongoose');
+const dbgr=require('debug')("development:mongoose");
+const config=require('config');
 
-mongoose.connect('mongodb://127.0.0.1:27017/shoppe').then(()=>console.log('Connected!')).catch((err)=>{
-    console.error(err);
+mongoose.connect(`${config.get("MONGODB_URI")}/shoppe`).then(()=>dbgr('Connected!')).catch((err)=>{
+    dbgr(err);
 })
 
 module.export=mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,28 @@
 const express=require('express');
 const router=express.Router();
+const ownerModel=require('../models/ownerModel');
+
+if(process.env.NODE_ENV==='development'){
+        router.post('/create',async (req,res)=>{
+        let owners=await ownerModel.find();
+        if(owners.length>0){
+            return res.status(503).send('You are not permitted to creating user!')
+        }
+        let {name,email,password}=req.body;
+        console.log(req.body);
+;        let owner=await ownerModel.create({
+            name,
+            email,
+            password,
+        })
+         res.status(201).send(owner);
+    })
+}
 
 router.get('/',(req,res)=>{
     res.send('Owner Router called /')
 })
+
+
 
 module.exports=router;
